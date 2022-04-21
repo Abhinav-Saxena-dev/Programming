98aedffc0524ad26faa2aecd8e6eef6f1b428634
Abhinav-Saxena-dev <abhinavsaxena148@gmail.com>
2022-04-21 23:51:48 +0530

    adding find duplicate in array which uses hare and tortoise method

---
+++ Testing/src/com/java/LubBabbar/Arrays/FindDuplicateNumberNplus1Array.java
@@ -0,0 +1,30 @@
+package com.java.LubBabbar.Arrays;
+
+import java.util.Scanner;
+
+public class FindDuplicateNumberNplus1Array {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int a[] = new int[n];
+        for(int i = 0; i < n; i++){
+            a[i] = sc.nextInt();
+        }
+        int res = findDuplicate(a);
+    }
+    public static int findDuplicate(int[] nums) {
+        int slow = nums[0];
+        int fast = nums[0];
+        do{
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        }while(slow != fast);
+
+        fast = nums[0];
+        while(slow != fast){
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+        return slow;
+    }
+}
